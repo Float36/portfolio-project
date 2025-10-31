4f21491e1128cae22c297202200aaccc9dee4efe
Yura <yurastashchyshyn@gmail.com>
2025-10-31 14:00:44 +0200

    Adding serializer for portfolio

---
+++ portfolio/serializers.py
@@ -0,0 +1,44 @@
+from rest_framework import serializers
+from .models import Technology, Project, Experience, Education
+
+
+class TechnologySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Technology
+        fields = ['id', 'name']
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    """
+        Тут ми хочемо показувати не просто ID технологій,
+        а повні об'єкти (завдяки 'TechnologySerializer(many=True)').
+        """
+    technologies = TechnologySerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Project
+        # 'profile' ми не включаємо в fields, бо всі проєкти
+        # і так належатимуть одному профілю.
+        # Ми будемо фільтрувати за профілем у View.
+        fields = [
+            'id', 'title', 'description', 'image', 'github_link',
+            'live_link', 'created_at', 'technologies'
+        ]
+
+
+class ExperienceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Experience
+        fields = ['id', 'company', 'role', 'start_date',
+            'end_date', 'description'
+        ]
+
+
+class EducationSerializers(serializers.ModelSerializer):
+    model = Education
+    fields = [
+        'id', 'institution', 'degree', 'field_of_study',
+        'start_date', 'end_date'
+    ]
+
+
