e50da7f358329ecdc565fdbf4e59bdd46209f8e4
Yura <yurastashchyshyn@gmail.com>
2025-10-31 16:08:11 +0200

    Adding a jwt-authentication

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import ( # <-- Імпортуйте це
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('api/v1/', include('config.api_router'))
+    path('api/v1/', include('config.api_router')),
+
+    # Endpoints for JWT
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
