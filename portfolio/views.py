bd857f65872159fe9fcf4ea8e57eeefe401d2a83
Yura <yurastashchyshyn@gmail.com>
2025-10-31 15:10:00 +0200

    Add simple views for portfolio and users

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Technology, Project, Experience, Education
+from .serializers import (
+    TechnologySerializer, ProjectSerializer,
+    ExperienceSerializer, EducationSerializers
+)
 
-# Create your views here.
+
+class TechnologyViewSet(viewsets.ModelViewSet):
+    """
+    Full CRUD
+    """
+    queryset = Technology.objects.all()
+    serializer_class = TechnologySerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class ExperienceViewSet(viewsets.ModelViewSet):
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
+    permission_classes = [permissions.AllowAny]
+
+
+class EducationViewSet(viewsets.ModelViewSet):
+    queryset = Education.objects.all()
+    serializer_class = EducationSerializers
+    permission_classes = [permissions.AllowAny]
+
+
